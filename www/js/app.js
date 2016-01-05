0e7f8e799ebf2c87d424e2bd23dd75b6cf337492
Katherine <slakat@gmail.com>
2016-01-04 22:27:38 -0300

    fb connection done

--- www/js/app.js
+++ www/js/app.js
@@ -1,85 +1,127 @@
-// Ionic Starter App
+angular.module('sociogram', ['ionic', 'openfb', 'sociogram.controllers'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+    .run(function ($rootScope, $state, $ionicPlatform, $window, OpenFB) {
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+        OpenFB.init('1528657274093741','http://localhost:8100/oauthcallback.html',$window.sessionStorage);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+        $ionicPlatform.ready(function () {
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
 
-.config(function($stateProvider, $urlRouterProvider) {
+        $rootScope.$on('$stateChangeStart', function(event, toState) {
+            if (toState.name !== "app.login" && toState.name !== "app.logout" && !$window.sessionStorage['fbtoken']) {
+                $state.go('app.login');
+                event.preventDefault();
+            }
+        });
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+        $rootScope.$on('OAuthException', function() {
+            $state.go('app.login');
+        });
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html",
+                controller: "AppCtrl"
+            })
 
-});
+            .state('app.login', {
+                url: "/login",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/login.html",
+                        controller: "LoginCtrl"
+                    }
+                }
+            })
+
+            .state('app.logout', {
+                url: "/logout",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/logout.html",
+                        controller: "LogoutCtrl"
+                    }
+                }
+            })
+
+            .state('app.profile', {
+                url: "/profile",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/profile.html",
+                        controller: "ProfileCtrl"
+                    }
+                }
+            })
+
+            .state('app.share', {
+                url: "/share",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/share.html",
+                        controller: "ShareCtrl"
+                    }
+                }
+            })
+
+            .state('app.friends', {
+                url: "/person/:personId/friends",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/friend-list.html",
+                        controller: "FriendsCtrl"
+                    }
+                }
+            })
+
+            .state('app.allfriends', {
+                url: "/person/:personId/allfriends",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/all-friend-list.html",
+                        controller: "AllFriendsCtrl"
+                    }
+                }
+            })
+
+            .state('app.mutualfriends', {
+                url: "/person/:personId/mutualfriends",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/mutual-friend-list.html",
+                        controller: "MutualFriendsCtrl"
+                    }
+                }
+            })
+            .state('app.person', {
+                url: "/person/:personId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/person.html",
+                        controller: "PersonCtrl"
+                    }
+                }
+            })
+            .state('app.feed', {
+                url: "/person/:personId/feed",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/feed.html",
+                        controller: "FeedCtrl"
+                    }
+                }
+            });
+
+        // fallback route
+        $urlRouterProvider.otherwise('/app/person/me/feed');
+
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,128 @@
-angular.module('starter.controllers', [])
+angular.module('sociogram.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('AppCtrl', function ($scope, $state, OpenFB) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+        $scope.logout = function () {
+            OpenFB.logout();
+            $state.go('app.login');
+        };
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+        $scope.revokePermissions = function () {
+            OpenFB.revokePermissions().then(
+                function () {
+                    $state.go('app.login');
+                },
+                function () {
+                    alert('OpenFB : Revoke Permissions Failed!');
+                });
+        };
+    })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    .controller('LoginCtrl', function ($scope, $location, OpenFB) {
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+        $scope.facebookLogin = function () {
+
+            OpenFB.login('email,public_profile,user_friends,user_photos,user_posts,publish_actions,user_birthday').then(
+                function () {
+                    $location.path('/app/person/me/feed');
+                },
+                function () {
+                    alert('OpenFB : Login Failed! Please Try Again...');
+                });
+        };
+    })
+
+    .controller('ShareCtrl', function ($scope, OpenFB) {
+
+        $scope.item = {};
+
+        $scope.share = function () {
+            OpenFB.post('/me/feed', $scope.item)
+                .success(function () {
+                    $scope.status = "OpenFB : Item Shared Successfully!";
+                })
+                .error(function(data) {
+                    alert(data.error.message);
+                });
+        };
+    })
+
+    .controller('ProfileCtrl', function ($scope, OpenFB) {
+        OpenFB.get('/me?fields=id,name,email,birthday').success(function (user) {
+            $scope.user = user;
+        });
+    })
+
+    .controller('PersonCtrl', function ($scope, $stateParams, OpenFB) {
+        OpenFB.get('/' + $stateParams.personId + '?fields=id,name,email,birthday').success(function (user) {
+            $scope.user = user;
+        });
+    })
+
+    .controller('FriendsCtrl', function ($scope, $stateParams, OpenFB) {
+        OpenFB.get('/' + $stateParams.personId + '/friends', {limit: 5})
+            .success(function (result) {
+                $scope.friends = result.data;
+            })
+            .error(function(data) {
+                alert(data.error.message);
+            });
+    })
+
+    .controller('AllFriendsCtrl', function ($scope, $stateParams, OpenFB) {
+        OpenFB.get('/' + $stateParams.personId + '/taggable_friends', {limit: 2000})
+            .success(function (result) {
+                $scope.friends = result.data;
+            })
+            .error(function(data) {
+                alert(data.error.message);
+            });
+    })
+
+    .controller('MutualFriendsCtrl', function ($scope, $stateParams, OpenFB) {
+        OpenFB.get('/' + $stateParams.personId + '?fields=context.fields%28mutual_friends%29', {limit: 50})
+            .success(function (result) {
+                OpenFB.get('/' + result.context.id, {limit: 50})
+                    .success(function (result) {
+                      $scope.friends = result.mutual_friends.data;
+                    })
+                    .error(function(data) {
+                        alert(data.error.message);
+                    });
+            })
+            .error(function(data) {
+                alert(data.error.message);
+            });
+    })
+
+    .controller('FeedCtrl', function ($scope, $stateParams, OpenFB, $ionicLoading) {
+
+        $scope.show = function() {
+            $scope.loading = $ionicLoading.show({
+                content: 'Loading User Feed(s)...'
+            });
+        };
+        $scope.hide = function(){
+            $scope.loading.hide();
+        };
+
+        function loadFeed() {
+            $scope.show();
+            OpenFB.get('/me/feed', {limit: 30})
+                .success(function (result) {
+                    $scope.hide();
+                    $scope.items = result.data;
+                    // Used with pull-to-refresh
+                    $scope.$broadcast('scroll.refreshComplete');
+                })
+                .error(function(data) {
+                    $scope.hide();
+                    alert(data.error.message);
+                });
+        }
+
+        $scope.doRefresh = loadFeed;
+
+        loadFeed();
+
+    });
